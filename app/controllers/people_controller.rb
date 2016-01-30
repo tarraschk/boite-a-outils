e7ea3ef72ed37e4a7a5c7d11990c98973cef6790
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-01-30 02:11:47 +0100

    Ajout tags à la création

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -78,15 +78,13 @@ class PeopleController < ApplicationController
   # POST /people.json
   def create
     @person = Person.new(person_params)
-
-    respond_to do |format|
-      if @person.save
-        format.html { redirect_to @person, notice: 'Person was successfully created.' }
-        format.json { render :show, status: :created, location: @person }
-      else
-        format.html { render :new }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
-      end
+    @person.parent_id = current_person.people_id
+    @person.tags = (JSON.parse(current_person.tags) & ["comite_membre", "comite_jeune", "comite_comite"])
+    @person.send_to_nation_builder
+    if @person.save
+      render json: @person
+    else
+      render json: @person.errors
     end
   end
 
