3d501bebe38fc2b3e52af70d7562bafd79d05efd
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-02-08 11:39:40 +0100

    Fix cas où parent_id est nil

--- app/workers/nation_builder_get_parent_id_worker.rb
+++ app/workers/nation_builder_get_parent_id_worker.rb
@@ -5,6 +5,6 @@ class NationBuilderGetParentIdWorker
     client = NationBuilderClient.new
     parent_id = client.call(:people, :show, id: people_id)['person'].slice('parent_id').merge(updated_from_nb: true)
 
-    Person.find_by(people_id: people_id).update(parent_id)
+    Person.find_by(people_id: people_id).update(parent_id) unless parent_id['parent_id'].blank?
   end
 end
