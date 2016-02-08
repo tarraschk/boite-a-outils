bf16f2d678347fba69678c9f8db34d53d01967a9
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-02-08 19:24:45 +0100

    Fix create or update People with Worker

--- app/workers/nation_builder_sync_worker.rb
+++ app/workers/nation_builder_sync_worker.rb
@@ -32,10 +32,38 @@ class NationBuilderSyncWorker
   def create_people_from_result(people_list)
     logger.info "       #############                    current people id #{people_list.first['id']}                 ################"
     puts        "       #############                    current people id #{people_list.first['id']}                 ################"
-    already_done = Person.where(people_id: people_list.map{|p| p['id']}).pluck(:people_id)
-    to_do = people_list.map {|person| person.slice(*%w(id email first_name last_name recruiter_id phone mobile parent_id tags support_level mandat home_address))}.map {|h| already_done.include?(h['id']) ? nil : (h['people_id'] = h.delete('id'); h)}.compact
-    ActiveRecord::Base.transaction do
-      Person.create(to_do)
+    #already_done = Person.where(people_id: people_list.map{|p| p['id']}).pluck(:people_id)
+    #to_do = people_list.map {|person| person.slice(*%w(id email first_name last_name recruiter_id phone mobile parent_id tags support_level mandat home_address))}.map {|h| already_done.include?(h['id']) ? nil : (h['people_id'] = h.delete('id'); h)}.compact
+    people_list.each do |person|
+      puts        "--- creating people id #{person['id']} ---"
+      ActiveRecord::Base.transaction do
+        Person.skip_callbacks = true
+        Person.where(people_id: person['id']).first_or_initialize do |instance_person|
+          instance_person.people_id = person['id']
+          instance_person.email = person['email']
+          instance_person.first_name = person['first_name']
+          instance_person.last_name = person['last_name']
+          instance_person.recruiter_id = person['recruiter_id']
+          instance_person.phone = person['phone']
+          instance_person.mobile = person['mobile']
+          instance_person.parent_id = person['parent_id']
+          instance_person.tags = person['tags']
+          instance_person.support_level = person['support_level']
+          instance_person.mandat = person['mandat']
+          instance_person.save
+
+          home_address ||= Address.new
+          if home_address = person['primary_address'] || person['home_address']
+            instance_person.home_address.address1 = home_address['address1']
+            instance_person.home_address.address2 = home_address['address2']
+            instance_person.home_address.address3 = home_address['address3']
+            instance_person.home_address.city     = home_address['city']
+            instance_person.home_address.zip      = home_address['zip']
+            instance_person.home_address.save
+          end
+        end
+        Person.skip_callbacks = false
+      end
     end
     ActiveRecord::Base.clear_active_connections!
   end
