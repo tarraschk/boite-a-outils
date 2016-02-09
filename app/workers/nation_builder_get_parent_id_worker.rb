33f9b8a4405cd08c5d5bdf5cbb37cfd464a952ef
Vincent <vincent@classnco.com>
2016-02-09 20:58:05 +0100

    Correct synchronisation callbacks AGAIIN

--- app/workers/nation_builder_get_parent_id_worker.rb
+++ app/workers/nation_builder_get_parent_id_worker.rb
@@ -8,8 +8,10 @@ class NationBuilderGetParentIdWorker
     parent_id = client.call(:people, :show, id: people_id)['person'].slice('parent_id').merge(updated_from_nb: true)
 
     Person.skip_get_parent_id_callbacks = true
+    Person.skip_callbacks               = true
     person.update(parent_id) unless parent_id['parent_id'].blank?
   ensure
     Person.skip_get_parent_id_callbacks = false
+    Person.skip_callbacks               = false
   end
 end
