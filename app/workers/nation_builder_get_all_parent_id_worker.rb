564ba4572ad794ba44a211d17793b3885f7000a2
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-02-10 12:22:27 +0100

    Create Worker GetAllParentId

---
+++ app/workers/nation_builder_get_all_parent_id_worker.rb
@@ -0,0 +1,14 @@
+class NationBuilderGetAllParentIdWorker
+  include Sidekiq::Worker
+
+  def perform(people_id)
+    Rails.logger.info "--- creating parent for people id #{people_id} ---"
+    puts "--- creating parent for people id #{people_id} ---"
+
+    if people_id > 0
+      person = Person.find_by(people_id: people_id)
+      person.get_parent_id if person
+      NationBuilderGetAllParentIdWorker.perform_async(people_id-1)
+    end
+
+end
