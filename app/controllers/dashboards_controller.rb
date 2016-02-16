0b5b63ae34ec0ce86f2f9689084b6bd166863313
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-02-16 21:33:44 +0100

    Ajout des recrutés

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < SignedInController
   def dashboard
     if user_signed_in?
-      @children = current_person.children
+      @children = current_person.children.to_a | current_person.recruitees.to_a
       @parent   = current_person.parent
     end
   end
