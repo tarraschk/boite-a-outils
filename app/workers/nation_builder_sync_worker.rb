fb2867654f67fd229bad7d8ea6ef83ef60518ddd
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-02-21 20:56:45 +0100

    Fix bug first_or_initiliaze

--- app/workers/nation_builder_sync_worker.rb
+++ app/workers/nation_builder_sync_worker.rb
@@ -37,28 +37,27 @@ class NationBuilderSyncWorker
       puts        "--- creating people id #{person['id']} ---"
       next unless person['email'] || person['phone'] || person['mobile']
 
-      Person.where(people_id: person['id']).first_or_initialize do |instance_person|
-        instance_person.people_id     = person['id']
-        instance_person.email         = person['email'].to_s
-        instance_person.first_name    = person['first_name'].to_s
-        instance_person.last_name     = person['last_name'].to_s
-        instance_person.recruiter_id  = person['recruiter_id']
-        instance_person.phone         = person['phone'].to_s
-        instance_person.mobile        = person['mobile'].to_s
-        instance_person.tags          = person['tags']
-        instance_person.support_level = person['support_level']
-        instance_person.mandat        = person['mandat']
-        instance_person.save_without_callbacks
+      instance_person = Person.where(people_id: person['id']).first_or_initialize
+      instance_person.people_id     = person['id']
+      instance_person.email         = person['email'].to_s
+      instance_person.first_name    = person['first_name'].to_s
+      instance_person.last_name     = person['last_name'].to_s
+      instance_person.recruiter_id  = person['recruiter_id']
+      instance_person.phone         = person['phone'].to_s
+      instance_person.mobile        = person['mobile'].to_s
+      instance_person.tags          = person['tags']
+      instance_person.support_level = person['support_level']
+      instance_person.mandat        = person['mandat']
+      instance_person.save_without_callbacks
 
-        instance_person.home_address ||= Address.new
-        if home_address = person['primary_address'] || person['home_address']
-          instance_person.home_address.address1 = home_address['address1']
-          instance_person.home_address.address2 = home_address['address2']
-          instance_person.home_address.address3 = home_address['address3']
-          instance_person.home_address.city     = home_address['city']
-          instance_person.home_address.zip      = home_address['zip']
-          instance_person.home_address.save_without_callbacks
-        end
+      instance_person.home_address ||= Address.new
+      if home_address = person['primary_address'] || person['home_address']
+        instance_person.home_address.address1 = home_address['address1']
+        instance_person.home_address.address2 = home_address['address2']
+        instance_person.home_address.address3 = home_address['address3']
+        instance_person.home_address.city     = home_address['city']
+        instance_person.home_address.zip      = home_address['zip']
+        instance_person.home_address.save_without_callbacks
       end
     end
     ActiveRecord::Base.clear_active_connections!
