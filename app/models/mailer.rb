63fc71fd3ded5dc9448fd657f17d6fbfc0593d30
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-03-07 22:34:31 +0100

    Fix mailer

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,16 +2,17 @@ class Mailer
 
   def send_error(e)
     Mail.defaults do
-      delivery_method :smtp, { :address              => "smtp.gmail.com",
-                               :port                 => 587,
-                               :user_name            => ENV['email_username'],
-                               :password             => ENV['email_password'],
-                               :authentication       => 'plain',
-                               :enable_starttls_auto => true  }
+      delivery_method :smtp,
+                      :address              => "smtp.gmail.com",
+                      :port                 => 587,
+                      :user_name            => ENV['EMAIL_USERNAME'],
+                      :password             => ENV['EMAIL_PASSWORD'],
+                      :authentication       => 'plain',
+                      :enable_starttls_auto => true
     end
     Mail.deliver do
       to "maxime.alayeddine@juppe-2017.fr"
-      from ENV['email_username']
+      from ENV['EMAIL_USERNAME']
       subject "[BOA] Erreur détectée"
       html_part do
         content_type 'text/html; charset=UTF-8'
