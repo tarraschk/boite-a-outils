366d2fbaf527b2f6eb9db5c6b3dd926ba2e96b55
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-03-07 22:34:49 +0100

    NB new SyncWorker

---
+++ app/workers/nation_builder_sync_launch_worker.rb
@@ -0,0 +1,36 @@
+class NationBuilderSyncLaunchWorker
+  include Sidekiq::Worker
+
+  def perform
+    last_synchronization = Synchronization.find_or_initialize_by(event: 'people')
+    updated_at = last_synchronization.updated_at
+    last_synchronization.updated_at = Time.now
+
+    client = NationBuilderClient.new
+    params = updated_at ? [:search, updated_since: updated_at, limit: 100] : [:index, limit: 100]
+    paginator = NationBuilder::Paginator.new(client, client.call(:people, *params))
+    while paginator.next?
+      paginator.body['results'].each do |result|
+        NationBuilderSyncWorker.perform_async(result['id'])
+      end
+      begin
+        paginator = paginator.next
+      rescue => e
+        Rails.logger.error e
+        Mailer.new.send_error e
+        retry
+      end
+    end
+
+    Synchronization.record_timestamps = false
+    last_synchronization.save
+
+  rescue => e
+    Rails.logger.error e
+    Mailer.new.send_error e
+  ensure
+    Synchronization.record_timestamps = true
+    sleep 600
+    NationBuilderSyncLaunchWorker.perform_async
+  end
+end
--- app/workers/nation_builder_sync_worker.rb
+++ app/workers/nation_builder_sync_worker.rb
@@ -1,67 +1,37 @@
 class NationBuilderSyncWorker
   include Sidekiq::Worker
 
-  def perform
-    last_synchronization = Synchronization.find_or_initialize_by(event: 'people')
-    updated_at = last_synchronization.updated_at
-    last_synchronization.updated_at = Time.now
+  def perform(people_id)
+    logger.info "       #############                    current people id #{people_id}                 ################"
+    puts        "       #############                    current people id #{people_id}                 ################"
+    puts        "--- creating people id #{people_id} ---"
 
-    client = NationBuilderClient.new
-    params = updated_at ? [:search, updated_since: updated_at, limit: 100] : [:index, limit: 100]
-    paginator = NationBuilder::Paginator.new(client, client.call(:people, *params))
-    while paginator.next?
-      create_people_from_result(paginator.body['results'])
-      begin
-        paginator = paginator.next
-      rescue => e
-        Rails.logger.error e
-        retry
-      end
+    instance_person = Person.where(people_id: people_id).first_or_initialize
+
+    instance_person.people_id     = person['id']
+    instance_person.email         = person['email'].to_s
+    instance_person.first_name    = person['first_name'].to_s
+    instance_person.last_name     = person['last_name'].to_s
+    instance_person.recruiter_id  = person['recruiter_id']
+    instance_person.phone         = person['phone'].to_s
+    instance_person.mobile        = person['mobile'].to_s
+    instance_person.tags          = person['tags']
+    instance_person.support_level = person['support_level']
+    instance_person.mandat        = person['mandat']
+    instance_person.save_without_callbacks
+
+    instance_person.home_address ||= Address.new
+    if home_address = person['primary_address'] || person['home_address']
+      instance_person.home_address.address1 = home_address['address1']
+      instance_person.home_address.address2 = home_address['address2']
+      instance_person.home_address.address3 = home_address['address3']
+      instance_person.home_address.city     = home_address['city']
+      instance_person.home_address.zip      = home_address['zip']
+      instance_person.home_address.save_without_callbacks
     end
-
-    Synchronization.record_timestamps = false
-    last_synchronization.save
 
   rescue => e
-    logger.info e
-  ensure
-    Synchronization.record_timestamps = true
-    sleep 600
-    NationBuilderSyncWorker.perform_async
-  end
-
-  def create_people_from_result(people_list)
-    logger.info "       #############                    current people id #{people_list.first['id']}                 ################"
-    puts        "       #############                    current people id #{people_list.first['id']}                 ################"
-    #already_done = Person.where(people_id: people_list.map{|p| p['id']}).pluck(:people_id)
-    #to_do = people_list.map {|person| person.slice(*%w(id email first_name last_name recruiter_id phone mobile parent_id tags support_level mandat home_address))}.map {|h| already_done.include?(h['id']) ? nil : (h['people_id'] = h.delete('id'); h)}.compact
-    people_list.each do |person|
-      puts        "--- creating people id #{person['id']} ---"
-      next unless person['email'] || person['phone'] || person['mobile']
-
-      instance_person = Person.where(people_id: person['id']).first_or_initialize
-      instance_person.people_id     = person['id']
-      instance_person.email         = person['email'].to_s
-      instance_person.first_name    = person['first_name'].to_s
-      instance_person.last_name     = person['last_name'].to_s
-      instance_person.recruiter_id  = person['recruiter_id']
-      instance_person.phone         = person['phone'].to_s
-      instance_person.mobile        = person['mobile'].to_s
-      instance_person.tags          = person['tags']
-      instance_person.support_level = person['support_level']
-      instance_person.mandat        = person['mandat']
-      instance_person.save_without_callbacks
-
-      instance_person.home_address ||= Address.new
-      if home_address = person['primary_address'] || person['home_address']
-        instance_person.home_address.address1 = home_address['address1']
-        instance_person.home_address.address2 = home_address['address2']
-        instance_person.home_address.address3 = home_address['address3']
-        instance_person.home_address.city     = home_address['city']
-        instance_person.home_address.zip      = home_address['zip']
-        instance_person.home_address.save_without_callbacks
-      end
-    end
-    ActiveRecord::Base.clear_active_connections!
+    Rails.logger.error e
+    Mailer.new.send_error e
   end
 end
