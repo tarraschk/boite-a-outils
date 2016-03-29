7628158e83421a89a1c1496d52aeaa8466c0507a
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-03-29 17:44:39 +0200

    Fix callbacks

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,11 +2,11 @@ class Address < ActiveRecord::Base
   belongs_to :person
   cattr_accessor :skip_callbacks
 
-  after_create  :send_to_nation_builder
-  after_save    :send_to_nation_builder, unless: :skip_callbacks
+  #after_create  :send_to_nation_builder, unless: :skip_callbacks
+  #after_save    :send_to_nation_builder, unless: :skip_callbacks
 
   def send_to_nation_builder
-    params = {primary_address: attributes.slice(*%w(address1 city zip))}
+    params = {primary_address: attributes.slice(*%w(address1 address2 address3 city zip))}
 
     if person.people_id
       client = NationBuilderClient.new
