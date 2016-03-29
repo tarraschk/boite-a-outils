02daa4c6e2a0973afa3a8a9b38340da25ca76012
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-03-29 21:03:51 +0200

    Ajout scope activated

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < SignedInController
   def dashboard
     if user_signed_in?
-      @children = current_person.children
+      @children = current_person.children.activated
       @parent   = current_person.parent
     end
   end
