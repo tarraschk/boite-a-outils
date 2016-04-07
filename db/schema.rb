9a5401330131d57bd11bb27da24003e9d4596c12
Maxime Alay-Eddine <maxime@cyberwatch.fr>
2016-04-07 21:37:23 +0200

    Intégration Admin Tools

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313212514) do
+ActiveRecord::Schema.define(version: 20160329172022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 20160313212514) do
     t.integer  "recruiter_id"
     t.integer  "user_id"
     t.string   "email"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "mobile"
     t.string   "first_name"
     t.string   "last_name"
@@ -67,8 +67,8 @@ ActiveRecord::Schema.define(version: 20160313212514) do
     t.string   "tags"
     t.string   "mandat"
     t.string   "phone"
+    t.boolean  "activated",            default: true
     t.string   "nation_builder_error"
-    t.boolean  "activated",       default: true
   end
 
   add_index "people", ["parent_id"], name: "index_people_on_parent_id", using: :btree
